9955653014a8bb7a0ce88cd10cdb9f662b739563
💭AndreyGalushko💻 <andrey.galushko@rutorika.ru>
2020-01-29 22:17:27 +0300

    добавил 404 страницу и CMS контроллер, чтобы от него наследоваться и не писать в каждом классе метод конструкт

--- engine/Cms.php
+++ engine/Cms.php
@@ -2,6 +2,7 @@
 // точка запуска моего приложения
 namespace Engine;
 
+use Engine\Core\Router\DispatchedRoute;
 use Engine\Helper\Common;
 
 class Cms
@@ -29,18 +30,27 @@ class Cms
      */
     public function run()
     {
-        $this->router->add('home', '/', 'HomeController:index');
-        $this->router->add('product', '/product/12', 'ProductController:index');
-        $this->router->add('product', '/news', 'HomeController:news');
+        try {
+            $this->router->add('home', '/', 'HomeController:index');
+            $this->router->add('product', '/product/12', 'ProductController:index');
+            $this->router->add('product', '/news', 'HomeController:news');
 
-        $routerDispatch = $this->router->dispatch(Common::getMethod(), Common::getPathUrl());
+            $routerDispatch = $this->router->dispatch(Common::getMethod(), Common::getPathUrl());
+
+            if ($routerDispatch == null) {
+                $routerDispatch = new DispatchedRoute('ErrorController:page404');
+            }
 
 
-        list($class, $action) = explode(':', $routerDispatch->getController(), 2);
+            list($class, $action) = explode(':', $routerDispatch->getController(), 2);
 
-        $controller = '\\Cms\\Controller\\' . $class;
-        call_user_func_array([new $controller($this->di), $action], $routerDispatch->getParameters());
+            $controller = '\\Cms\\Controller\\' . $class;
+            $parameters = $routerDispatch->getParameters();
+            call_user_func_array([new $controller($this->di), $action], $parameters);
+        } catch (\Exception $e){
 
-        //print_r($routerDispatch);
+            echo $e->getMessage();
+            exit;
+        }
     }
 }
