92ad1c22c9a0c25d6e041c03f89588c9bd7c9091
💭AndreyGalushko💻 <andrey.galushko@rutorika.ru>
2020-01-31 22:37:21 +0300

    добавил папку admin добавил константу для подключения классов роутера и контроллера

--- admin/index.php
+++ admin/index.php
@@ -2,5 +2,5 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 define('ROOT_DIR', __DIR__);
-define('ENV', 'Cms');
-require_once 'engine/bootstrap.php';
+define('ENV', 'Admin');
+require_once '../engine/bootstrap.php';
--- engine/Cms.php
+++ engine/Cms.php
@@ -33,7 +33,7 @@ class Cms
     {
         try {
 
-            require_once __DIR__ . '/../cms/Route.php';
+            require_once __DIR__ . '/../' . mb_strtolower(ENV) . '/Route.php';
 
             $routerDispatch = $this->router->dispatch(Common::getMethod(), Common::getPathUrl());
 
@@ -44,7 +44,7 @@ class Cms
 
             list($class, $action) = explode(':', $routerDispatch->getController(), 2);
 
-            $controller = '\\Cms\\Controller\\' . $class;
+            $controller = '\\' . ENV . '\\Controller\\' . $class;
             $parameters = $routerDispatch->getParameters();
             call_user_func_array([new $controller($this->di), $action], $parameters);
         } catch (\Exception $e){
