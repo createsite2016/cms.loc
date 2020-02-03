3d57adca1037cb7308aad00120bb632dfa5f7f3c
💭AndreyGalushko💻 <andrey.galushko@rutorika.ru>
2020-02-03 23:42:08 +0300

    сделал класс проверки авторизации пользователя

--- admin/Controller/AdminController.php
+++ admin/Controller/AdminController.php
@@ -3,9 +3,15 @@
 namespace Admin\Controller;
 
 use Engine\Controller;
+use Engine\Core\Auth\Auth;
 
 class AdminController extends Controller
 {
+    /**
+     * @var Auth
+     */
+    protected $auth;
+
     /**
      * AdminController constructor.
      * @param \Engine\DI\DI $di
@@ -13,5 +19,32 @@ class AdminController extends Controller
     public function __construct($di)
     {
         parent::__construct($di);
+
+        $this->auth = new Auth();
+
+        $this->checkAuthorization();
+
+        if(isset($this->request->get['logout']))
+        {
+            $this->auth->unAuthorize();
+        }
+    }
+
+    /**
+     *  Проверка авторизации пользователя
+     */
+    public function checkAuthorization()
+    {
+        if($this->auth->hashUser() !== null)
+        {
+            $this->auth->authorize($this->auth->hashUser());
+        }
+
+        if(!$this->auth->authorized())
+        {
+            // если пользователь не авторизован
+            header('Location: /admin/login/', true, 301);
+            exit;
+        }
     }
 }
